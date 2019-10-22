d09065f14790f6a81bba561f83f477c7b07fb2d4
Andrew Silkroski <andrew.silkroski@gmail.com>
2019-10-22 09:55:11 -0700

    Create brew_setup.sh

---
+++ brew_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+##
+# Install Brew and some useful packages on Mac OS
+# Can (and should) be used with other bootstrapping scripts/processes
+# Make sure to add the 'chmod +x' for this to run
+##
+
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Installing brew cask..."
+brew tap caskroom/cask
+
+# Development / MgmT
+echo "Installing Development and Management items..."
+brew install ansible
+brew install awscli
+brew install git
+brew install docker
+brew install wget
+
+# Other stuff
+echo "almost done..."
+brew cask install google-chrome
+brew cask install slack
+
+# Cleanup Section
+brew clean
